ebb66c6f560897e56e7f76f35dbc989393cc270a
Tor-Einar Skog <tor-einar.skog@nibio.no>
2021-11-09 12:57:10 +0100

    Adding the crontab compatible script example

---
+++ run_import_data_example.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+ 
+ #    Copyright (C) 2021  Tor-Einar Skog,  NIBIO
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+# Use this as a template for running the import_data.py script
+# from crontab
+ 
+ 
+ APP_HOME=[YOUR_PATH_TO_EUROWEATHER-FRONTEND]
+ 
+ source $APP_HOME/venv/bin/activate
+ python3 $APP_HOME/app/batch/import_data.py
+
