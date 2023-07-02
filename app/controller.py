ca37c3fd3cf7d5084c11282f1fa6a45a01eebb68
Tor-Einar Skog <tor-einar.skog@nibio.no>
2023-07-02 08:30:17 +0200

    Reintroduce DEBUG output

--- app/controller.py
+++ app/controller.py
@@ -107,18 +107,19 @@ class Controller:
             cur.execute("DELETE FROM weather_data WHERE site_id=%s",(site_id,))
             data = weather_data.locationWeatherData[0].data
             #print(data)
-            #if DEBUG:
-            #    print("site_id=%s, time_measured=%s, parameter_id=%s, val=%s" % (
-            #        site_id, 
-            #        weather_data.timeStart + (3600*idy),
-            #        weather_data.weatherParameters[idx],
-            #        col
-            #        )
-            #    )
-            #
+            
             for idy, row in enumerate(data):
                 idx = 0
                 for col in row:
+                    if DEBUG:
+                        print("site_id=%s, time_measured=%s, parameter_id=%s, val=%s" % (
+                            site_id, 
+                            weather_data.timeStart + (3600*idy),
+                            weather_data.weatherParameters[idx],
+                            col
+                            )
+                        )
+                    
                     cur.execute(insert_tpl,({
                         "site_id": site_id,
                         "log_interval": 3600,
